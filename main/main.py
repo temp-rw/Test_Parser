8896f0a4e54ca78e9eba90995d9ee20e13402fe5
Artiom Daniuk <tema200100@gmail.com>
2020-10-02 12:10:04 +0300

    Add comments, write to file, tuiru parse

--- main/main.py
+++ main/main.py
@@ -1,11 +1,15 @@
 import json
+from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
 
 
-# Parse shop information from https://www.mebelshara.ru/contacts
-def parse_mebelshara(html) -> str:
+MEBELSHARA_URL = "https://www.mebelshara.ru/contacts"
+
+
+# Parse html from https://www.mebelshara.ru/contacts
+def parse_mebelshara(html):
     shops_info = []
     bs_obj = BeautifulSoup(html, features="lxml")
     city_list = bs_obj.findAll("div", {"city-item"})
@@ -22,26 +26,69 @@ def parse_mebelshara(html) -> str:
             }
             shops_info.append(shop_info)
 
-
     return json.dumps(shops_info, ensure_ascii=False, indent=2)
 
 
-def parse_tui(html):
-    pass
+# Generator return id of cities on tui.ru
+def get_tui_cities():
+    response = requests.get("https://apigate.tui.ru/api/office/cities")
+    cities = json.loads(response.text).get('cities')
+    for city in cities:
+        yield city.get('cityId')
 
+
+# Parse information using tui.ru API
+def parse_tui():
+    tui_offices = []
+    file = open("tuiru.json", 'w')
+    for cityId in get_tui_cities():
+        response = requests.get(f"https://apigate.tui.ru/api/office/list?CityId={cityId}")
+        offices = json.loads(response.text).get('offices')
+
+        for office in offices:
+            working_hours = []
+            workdays = office.get('hoursOfOperation').get('workdays')
+            saturday = office.get('hoursOfOperation').get('saturday')
+            sunday = office.get('hoursOfOperation').get('sunday')
+
+            if not workdays.get('isDayOff'):
+                working_hours.append(f"пн - пт {workdays.get('startStr')} до {workdays.get('endStr')}")
+
+            if not (saturday.get('isDayOff') and sunday.get('isDayOff')) and \
+                    saturday.get('startStr') == sunday.get('startStr') and \
+                    saturday.get('endStr') == sunday.get('endStr'):
+                working_hours.append(f"cб - вс {saturday.get('startStr')} до {saturday.get('endStr')}")
+
+            elif not saturday.get('isDayOff'):
+                working_hours.append(f"cб {saturday.get('startStr')} до {saturday.get('endStr')}")
+
+            elif not sunday.get('isDayOff'):
+                working_hours.append(f"cб {sunday.get('startStr')} до {sunday.get('endStr')}")
+
+            office_info = {
+                'address': office.get('address'),
+                'latlon': [office.get('latitude'), office.get('longitude')],
+                'name': office.get('name'),
+                'phones': [phone.get('phone').strip() for phone in office.get('phones')],
+                'working_hours': working_hours
+            }
+
+            tui_offices.append(office_info)
+
+        sleep(0.5)
+
+    return json.dumps(tui_offices, ensure_ascii=False, indent=2)
+
+
+# Write data to json file
+def write_json(file_name, data):
+    with open(file_name, 'w', encoding="utf-8") as file:
+        file.write(data)
 
 
 def main():
-    session = requests.Session()
-    session.headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
-        "Accept-Language": "ru-RU,ru-BY;q=0.9,ru;q=0.8,en-GB;q=0.7,en;q=0.6,be-BY;q=0.5,be;q=0.4,en-US;q=0.3"
-    }
-    mebelshara_url = "https://www.mebelshara.ru/contacts"
-    tui_url = "https://www.tui.ru/offices/"
-    print(parse_mebelshara(session.get(mebelshara_url).text))
-    # parse_tui(requests.get(tui_url).text)
-
+    write_json("mebelshara.json", parse_mebelshara(requests.get(MEBELSHARA_URL).text))
+    write_json("tuiru.json", parse_tui())
 
 
 if __name__ == '__main__':
